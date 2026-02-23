fd95da4d517bfe1d3d6cbfc83a0d4ea13ac7cfdf
rsvainio <rasmus.vainio@gmail.com>
2026-02-23 14:50:56 +0200

    Added heap implementation for the purpose of future pathfinding

---
+++ Assets/Scripts/Heap.cs
@@ -0,0 +1,115 @@
+using System;
+
+public class Heap<T> where T : IHeapItem<T>
+{
+    public int Count => _currentItemCount;
+
+    private T[] _heap;
+    private int _currentItemCount;
+
+    public T Parent(T item) => (item.heapIndex - 1) / 2 == 1 ? default : _heap[(item.heapIndex - 1) / 2];
+    public T Left(T item) => item.heapIndex * 2 + 1 <= _currentItemCount ? _heap[item.heapIndex * 2 + 1] : default;
+    public T Right(T item) => item.heapIndex * 2 + 2 <= _currentItemCount ? _heap[item.heapIndex * 2 + 2] : default;
+
+    public Heap(int maxHeapSize)
+    {
+        _heap = new T[maxHeapSize];
+    }
+
+    public void Insert(T item)
+    {
+        item.heapIndex = _currentItemCount;
+        _heap[item.heapIndex] = item;
+        SortUp(item);
+        _currentItemCount++;
+    }
+
+    public T ExtractFirst()
+    {
+        T firstItem = _heap[0];
+        _currentItemCount--;
+
+        _heap[0] = _heap[_currentItemCount];
+        _heap[0].heapIndex = 0;
+        SortDown(firstItem);
+
+        return firstItem;
+    }
+
+    private void SortDown(T item)
+    {
+        while (true)
+        {
+            T leftChild = Left(item);
+            T rightChild = Right(item);
+            T itemToSwap;
+
+            if (leftChild.heapIndex < _currentItemCount)
+            {
+                itemToSwap = leftChild;
+
+                if (rightChild.heapIndex < _currentItemCount)
+                {
+                    if (leftChild.CompareTo(rightChild) < 0)
+                    {
+                        itemToSwap = rightChild;
+                    }
+                }
+
+                // swap the items if item is lower than itemToSwap
+                if (item.CompareTo(itemToSwap) < 0)
+                {
+                    Swap(item, itemToSwap);
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    private void SortUp(T item)
+    {
+        while (true)
+        {
+            T parent = Parent(item);
+
+            if (item.CompareTo(parent) > 0) // need to check for null
+            {
+                Swap(item, parent);
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    private void Swap(T itemA, T itemB)
+    {
+        _heap[itemA.heapIndex] = itemB;
+        _heap[itemB.heapIndex] = itemA;
+
+        (itemA.heapIndex, itemB.heapIndex) = (itemB.heapIndex, itemA.heapIndex);
+    }
+}
+
+public class PathNode : IHeapItem<PathNode>
+{
+    public int heapIndex { get; set; }
+
+    public int CompareTo(PathNode other)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public interface IHeapItem<T> : IComparable<T>
+{
+    int heapIndex { get; set; }
+}
